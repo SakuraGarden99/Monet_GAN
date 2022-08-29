8103e6318791d14932afe68a3bf1d70293e80d13
Aazar <shaikhaazar11@gmail.com>
2022-08-29 20:51:09 +0530

    loading dataset function added

--- preprocessing_tfrecord.py
+++ preprocessing_tfrecord.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
 #Getting features from tf_record files of dataset
 def list_record_features(tfrecords_path):
     # Dict of extracted feature information
@@ -40,24 +42,44 @@ image_feature_description={
     'target' : tf.io.FixedLenFeature([], tf.int64)
 }
 
-def parse_image_function(example_proto):
-	"""Only returning images after parsing"""
-	example = tf.io.parse_single_example(example_proto, image_feature_description)
-	image = example["image"]
 
-IMAGE_SIZE = [256, 256]
 def decode_image(image):
     image = tf.image.decode_jpeg(image, channels=3)
     image = (tf.cast(image, tf.float32) / 127.5) - 1
     image = tf.reshape(image, [*IMAGE_SIZE, 3])
     return image
 
-raw_dataset = tf.data.TFRecordDataset(filename)
-parsed_image_dataset = raw_dataset.map(parse_image_function)
-print(parsed_image_dataset)
+def parse_image_function(example_proto):
+	"""Only returning images after parsing"""
+	example = tf.io.parse_single_example(example_proto, image_feature_description)
+	image = decode_image(example["image"])
+	return image
 
-for sample in parsed_image_dataset.take(1):
-  image = decode_image(sample['image'])
-  plt.imshow(image)
+
+
+def load_dataset(filenames):
+  dataset = tf.data.TFRecordDataset(filename)
+  dataset = dataset.map(parse_image_function, num_parallel_calls=AUTOTUNE)
+  return dataset
+
+monet_ds= load_dataset("MONET/TFRECORD_PATH.tfrecord").batch(1)
+photo_ds = load_dataset("PHOTO/TFRECOED_PATH.tfrecord").batch(1)
+
+
+for sample in monet_ds.take(1):
+  plt.imshow(sample[0])
   plt.axis('off')
+
+# or
+example_monet_image = next(iter(monet_ds))
+example_photo_image = next(iter(photo_ds))
+
+plt.subplot(121)
+plt.title('Photo')
+plt.imshow(example_monet_image[0] * 0.5 + 0.5)
+
+plt.subplot(122)
+plt.title('Monet')
+plt.imshow(example_photo_image[0] * 0.5 + 0.5)
+
 
