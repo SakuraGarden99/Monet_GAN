12bc25292ae568066b4e461270859816ecd5dfec
Aazar <shaikhaazar11@gmail.com>
2022-10-09 15:04:44 +0530

    Completed all functions

--- CycleGAN/discriminator.py
+++ CycleGAN/discriminator.py
@@ -1,7 +1,30 @@
-import tensorflow as tf
-import glob
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import PIL
-from tensorflow.keras import layers
+def discriminator(target= True):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  inp= layers.Input(shape=[None, None, 3], name='input_image')
+  x= inp
+
+  if target:
+    tar = layers.Input(shape=[None, None, 3], name='target_image')
+    x= layers.Concatenate([inp,tar])
+
+  down1= downsample(64,4, apply_norm=False)(x)
+  down2= downsample(128,4)(down1)
+  down3= downsample(256, 4)(down2)
+
+  zero_pad1= layers.ZeroPadding2D()(down3)
+
+  conv= layers.Conv2D(512, 4, strides=1, kernel_initializer=initializer, use_bias=False)(zero_pad1)
+
+  norm1= InstanceNormalization()(conv)
+
+  leaky_relu= layers.LeakyReLU()(norm1)
+
+  zero_pad2= layers.ZeroPadding2D()(leaky_relu)
+
+  last = layers.Conv2D(1, 4, strides= 1, kernel_initializer=initializer)(zero_pad2)
+
+  if target:
+    return keras.Model(inputs= [inp, last], outputs= last)
+  else:
+    return keras.Model(inputs= inp, outputs= last)
--- CycleGAN/generator.py
+++ CycleGAN/generator.py
@@ -1,7 +1,44 @@
-import tensorflow as tf
-import glob
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import PIL
-from tensorflow.keras import layers
+def unet_generator(output_channels=3):
+  down_stack=[
+      downsample(64,4, apply_norm=False),
+      downsample(128,4),
+      downsample(256,4),
+      downsample(512,4),
+      downsample(512,4),
+      downsample(512,4),
+      downsample(512,4),
+      downsample(512,4),
+  ]
+
+  up_stack = [
+      upsample(512, 4, apply_dropout=True),
+      upsample(512, 4, apply_dropout=True),
+      upsample(512, 4, apply_dropout=True),
+      upsample(512, 4),
+      upsample(256, 4),
+      upsample(128, 4),
+      upsample(64, 4),
+  ]
+
+  initializer = tf.random_normal_initializer(0., 0.02)
+  last = layers.Conv2DTranspose(output_channels, 4, strides=2, padding='same', kernel_initializer= initializer, activation= 'tanh')
+
+  concat = layers.Concatenate()
+  inputs= layers.Input(shape=[None, None, 3])
+
+  x= inputs
+
+  skips= []
+  for down in down_stack:
+    x = down(x)
+    skips.append(x)
+  skips = reversed(skips[:-1])
+
+
+  for up,skip in zip(up_stack, skips):
+    x= up(x)
+    x= concat([x, skip])
+
+  x= last(x)
+
+  return keras.Model(inputs= inputs, outputs= x)
